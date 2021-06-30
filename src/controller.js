f5813a397822937593811ece4b35abc9ea6fc7aa
thaske <10328778+thaske@users.noreply.github.com>
2021-06-30 18:29:25 -0400

    Create controller.js

---
+++ src/controller.js
@@ -0,0 +1,24 @@
+import page from "~/page";
+import parse from "~/parse";
+import transform from '~/transform';
+
+const index = async (req, res) => {
+  try {
+    const { a, url } = req.query;
+
+    if(!url) return res.sendStatus(400);
+    const showAmenities = a;
+
+    const html = await page(url);
+    const data = transform(parse(html), Boolean(showAmenities));
+
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error(err.stack);
+    return res.sendStatus(400);
+  }
+};
+
+export default {
+  index
+};
