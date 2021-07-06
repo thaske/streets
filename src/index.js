d36f98e690e758beceb55947da41f0ca9a36c2dd
thaske <10328778+thaske@users.noreply.github.com>
2021-07-05 22:41:32 -0400

    Added API caching

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express, { json, urlencoded } from "express";
+import { middleware as cache } from 'apicache';
 import controller from "~/controller";
 
 const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
+app.use(cache("1 week"));
 app.get("/", controller.index);
 
 app.listen(3000, () =>
