5e632e218f07001b53971c4a006b0f76101458b1
thaske <10328778+thaske@users.noreply.github.com>
2021-09-19 14:24:57 -0400

    Updated name and removed unnecessary await

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,4 @@
-import page from "~/page";
+import fetchPage from "~/page";
 import parse from "~/parse";
 import transform from '~/transform';
 
@@ -9,10 +9,11 @@ const index = async (req, res) => {
     if(!url) return res.sendStatus(400);
     const showAmenities = Boolean(a);
 
-    const html = await page(url);
+    const html = await fetchPage(url);
     const data = transform(parse(html), showAmenities);
 
     return res.status(200).json(data);
+    
   } catch (err) {
     console.error(err.stack);
     return res.sendStatus(400);
--- src/page.js
+++ src/page.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import headers from "./headers.json";
 
-const page = async (url) => (
-  await axios.get(url, { headers }).then(res => res.data).catch(error => { throw new Error(error.message) })
+const fetchPage = async (url) => (
+  axios.get(url, { headers }).then(res => res.data).catch(error => { throw new Error(error.message) })
 )
 
-export default page;
+export default fetchPage;
